7a117c5a10db03ffb6d1f26aa26af4ee9839ffb9
Nishu Goel <n.goel@epilot.cloud>
2024-07-25 16:57:07 +0200

    regenerate price resource to accommodate price_components and fix unit price

--- examples/resources/epilot-product_product/resource.tf
+++ examples/resources/epilot-product_product/resource.tf
@@ -3,8 +3,9 @@ resource "epilot-product_product" "my_product" {
   code              = "...my_code..."
   description       = "...my_description..."
   internal_name     = "...my_internal_name..."
-  name              = "Juan Morar Jr."
+  name              = "Bradford Osinski"
   product_downloads = "{ \"see\": \"documentation\" }"
   product_images    = "{ \"see\": \"documentation\" }"
+  product_id        = "123e4567-e89b-12d3-a456-426614174000"
   type              = "product"
 }
