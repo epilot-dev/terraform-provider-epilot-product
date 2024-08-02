9fc546761146f92cc3707e650e3620e36b19d07b
Mateus Nardo <m.nardo@epilot.cloud>
2024-08-02 13:50:40 +0200

    chore: revert tax rate change

--- examples/resources/epilot-product_tax/resource.tf
+++ examples/resources/epilot-product_tax/resource.tf
@@ -1,7 +1,7 @@
 resource "epilot-product_tax" "my_tax" {
   active      = false
   description = "...my_description..."
-  rate        = "{ \"see\": \"documentation\" }"
+  rate        = "...my_rate..."
   region      = "AT"
   tax_id      = "123e4567-e89b-12d3-a456-426614174000"
   type        = "VAT"
